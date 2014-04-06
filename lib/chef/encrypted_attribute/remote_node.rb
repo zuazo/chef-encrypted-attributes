c6fd517467f4c06ddbed1373744438bfcb07bc08
Xabier de Zuazo <xabier@onddo.com>
2014-04-06 23:04:07 +0200

    Commented code removed from RemoteNode

--- lib/chef/encrypted_attribute/remote_node.rb
+++ lib/chef/encrypted_attribute/remote_node.rb
@@ -16,11 +16,6 @@ class Chef
       def load_attribute(attr_ary)
         keys = { 'value' => attr_ary }
         self.class.search(:node, "name:#{@name}", keys, 1)[0]['value']
-        # escaped_query = escape("name:#{@name}", Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-        # node = query.search(:node, escaped_query, nil, 0, 1)[0][0]
-        # attr_ary.reduce(node) do |n, k|
-        #   n.respond_to?(:has_key?) && n.has_key?(k) ? n[k] : nil
-        # end
       end
 
     end
