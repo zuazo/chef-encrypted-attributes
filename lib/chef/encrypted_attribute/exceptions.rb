122788963b5fb15715a9d2fcad57b21bfab94164
Xabier de Zuazo <xabier@onddo.com>
2014-04-16 11:09:32 +0200

    Added license header to all source files

--- lib/chef/encrypted_attribute/exceptions.rb
+++ lib/chef/encrypted_attribute/exceptions.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Xabier de Zuazo (<xabier@onddo.com>)
-# Copyright:: Copyright (c) 2014 Onddo Labs, SL.
+# Copyright:: Copyright (c) 2014 Onddo Labs, SL. (www.onddo.com)
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
